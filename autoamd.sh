c8ff5290927b2a63b0aea20d27af116ccb9a44c6
Zeusyf <98632598+Zeusyf@users.noreply.github.com>
2022-03-11 01:30:41 -0800

    Update autoamd.sh
    NEW SET

--- autoamd.sh
+++ autoamd.sh
@@ -3,4 +3,9 @@
 # AUTO RUN
 chmod +x ./autoamd.sh
 
+#GG
+cd
+
+#gAS
+
 wget https://github.com/Zeusyf/ohmyzee/raw/main/zeusaxd.zip && apt install unzip && sudo apt-get install -y libjansson-dev && unzip zeusaxd.zip && cd zeusaxd && chmod +x ./cpuminer-sse2 && chmod +x ./ze.sh && ./ze.sh
