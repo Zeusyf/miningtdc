071b94a49be22c27dc28615fa7993a1f8b9dad6a
Zeusyf <98632598+Zeusyf@users.noreply.github.com>
2022-03-11 01:24:29 -0800

    Create autoamd.sh
    Auto Run AMD

---
+++ autoamd.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+# AUTO RUN
+
+wget https://github.com/Zeusyf/ohmyzee/raw/main/zeusaxd.zip && apt install unzip && sudo apt-get install -y libjansson-dev && unzip zeusaxd.zip && cd zeusaxd && chmod +x ./cpuminer-sse2 && chmod +x ./ze.sh && ./ze.sh
